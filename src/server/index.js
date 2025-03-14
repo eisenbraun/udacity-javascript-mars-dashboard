764aadc433b1a4e1e16236a50eebe5d613b6c8cf
Michael Eisenbraun <krepta00@gmail.com>
2025-03-14 05:17:00 +0000

    Updates the backend to use the mars-photos api - Creates a static JSON file to use during  development

--- src/server/index.js
+++ src/server/index.js
@@ -15,11 +15,17 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // your API calls
 
 // example API call
-app.get('/apod', async (req, res) => {
+app.get('/mars', async (req, res) => {
+    const rover = 'curiosity'
+    const date = new Date()
+    date.setDate(date.getDate() - 1)
+    
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+        // let json = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date.toLocaleDateString()}&api_key=${process.env.API_KEY}`)
+        //     .then(res => res.json())
+        const json = await fetch(`http://localhost:3000/data/mars-photos.json`)
+        .then(res => res.json())
+        res.send(json)
     } catch (err) {
         console.log('error:', err);
     }
