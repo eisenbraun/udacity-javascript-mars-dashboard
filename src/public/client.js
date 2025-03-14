c4bda5602e403484aac5925039c6aff31badae26
Michael Eisenbraun <krepta00@gmail.com>
2025-03-14 05:21:59 +0000

    Updates Client.js - Retrieves Mar Photo data from backend - Uses retrieved data to create gallery - Dynamically creates Form - Add event listener for rover selection

--- src/public/client.js
+++ src/public/client.js
@@ -1,6 +1,6 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
+const store = {
+    photos: [],
+    rover: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
 
@@ -18,29 +18,8 @@ const render = async (root, state) => {
 
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
+const App = (state) => {    
+    return `${Form(state)} ${Photos(state)}`
 }
 
 // listening for load event because page should load before any JS is called
@@ -48,58 +27,49 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
+document.body.addEventListener('change', (e) => {
+    updateStore(store, {'rover': e.target.value})
+})
+
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
+const Form = (state) => {
+    return (`
+    <form class="p-5 bg-light border border-1 mb-3">
+        <div class="form-group">
+            <label class="form-label">Rovers</label>
+            <select class="form-select" id="rover">
+                <option>Select a Rover</option>
+                ${state.rovers.map(rover => 
+                    `<option ${rover === state.rover ? 'selected' : ''}>${rover}</option>`
+                ).join('')}
+            </select>
+        </div>
+    </form>    
+    `)
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+const Photos = (state) => {
+    if (state.photos.length === 0) {
+        getPhotos(state)
     }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+    
+    return (`
+    <h2 class="mb-3">Latest Photos</h2>
+    <div class="gallery">
+        ${state.photos.reduce((html, photo) => 
+            (html + `<img src="${photo.img_src}" alt="${photo.full_name}">`), '')}
+    </div>`)
 }
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
+const getPhotos = (state) => {
+    // const { photos } = state
+    
+    fetch(`http://localhost:3000/mars`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
+        .then(json => {
+            updateStore(state, json) 
+        })
 }
