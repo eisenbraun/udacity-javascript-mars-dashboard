8a42e81b91f4104d4fdafe47abae81d1678c48b2
Michael Eisenbraun <krepta00@gmail.com>
2025-04-06 15:14:05 -0400

    Updates backend - request now include specific rover - retrieves manifest to get latest photo date

--- src/server/index.js
+++ src/server/index.js
@@ -15,16 +15,18 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // your API calls
 
 // example API call
-app.get('/mars', async (req, res) => {
-    const rover = 'curiosity'
+app.get('/mars/:rover', async (req, res) => {
+    const rover = req.params.rover
     const date = new Date()
     date.setDate(date.getDate() - 1)
-    
+
     try {
-        // let json = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date.toLocaleDateString()}&api_key=${process.env.API_KEY}`)
-        //     .then(res => res.json())
-        const json = await fetch(`http://localhost:3000/data/mars-photos.json`)
-        .then(res => res.json())
+        const manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        const date = manifest.photo_manifest.max_date
+   
+        const json = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
         res.send(json)
     } catch (err) {
         console.log('error:', err);
